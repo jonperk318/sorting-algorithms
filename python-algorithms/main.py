4f36aff33a855572eadc9f1037d2f58c12d5a781
Jonathan Perkins <jperkin4@uab.edu>
2024-07-12 23:59:49 -0500

    "all" python script to store functions, and

--- python-algorithms/main.py
+++ python-algorithms/main.py
@@ -3,6 +3,9 @@ This script tests all 8 sorting algorithms using the randomly generated input fi
 and stores their run times in a text file to be plotted elsewhere
 """
 
+# importing all sorting functions from "all.py"
+from all import *
+
 import os
 import time
 
@@ -18,14 +21,63 @@ for s in sample_sizes: # Create a list of lists of input samples
 
     input = input.split(",")
 
-    if input[-1] == "":
+    if input[-1] == "": # if last value in array is empty, remove it
         input.pop()
-    input = [eval(i) for i in input]
+    input = [eval(i) for i in input] # get int from string
 
     samples.append(input)
 
 (insertion_times, selection_times, merge_times, quick_times,
  heap_times, counting_times, radix_times, bubble_times) = [], [], [], [], [], [], [], []
 
-print(heap_times)
+for s in samples:
 
+    # Insertion sort
+    tick = time.perf_counter()
+    insertion_sort(s)
+    tock = time.perf_counter()
+    insertion_times.append(tock - tick)
+
+    # Selection sort
+    tick = time.perf_counter()
+    selection_sort(s)
+    tock = time.perf_counter()
+    selection_times.append(tock - tick)
+
+    # Merge sort
+    tick = time.perf_counter()
+    merge_sort(s)
+    tock = time.perf_counter()
+    merge_times.append(tock - tick)
+
+    # Quick sort
+    tick = time.perf_counter()
+    quick_sort(s)
+    tock = time.perf_counter()
+    quick_times.append(tock - tick)
+
+    # Heap sort
+    tick = time.perf_counter()
+    heap_sort(s)
+    tock = time.perf_counter()
+    heap_times.append(tock - tick)
+
+    # Counting sort
+    tick = time.perf_counter()
+    counting_sort(s)
+    tock = time.perf_counter()
+    counting_times.append(tock - tick)
+
+    # Radix sort
+    tick = time.perf_counter()
+    radix_sort(s)
+    tock = time.perf_counter()
+    radix_times.append(tock - tick)
+
+    # Bubble sort
+    tick = time.perf_counter()
+    bubble_sort(s)
+    tock = time.perf_counter()
+    bubble_times.append(tock - tick)
+
+    print("Finishing sorting array of length " + str(s))
--- python-algorithms/test.py
+++ python-algorithms/test.py
@@ -1,11 +1,9 @@
-from insertion_sort import insertion_sort
-from merge_sort import merge_sort
-from quick_sort import quick_sort
-from selection_sort import selection_sort
-from heap_sort import heap_sort
-from counting_sort import counting_sort
-from radix_sort import radix_sort
-from bubble_sort import bubble_sort
+"""
+Testing all sorting functions to verify their ability to sort correcly
+"""
+
+# importing all sorting functions from "all.py"
+from all import * 
 
 test_arr = [3, 6, 7, 2, 4, 90, 43, 103, 2, 30234, 56, 5, 3, 2, 76, 43, 34, 102, 9]
 
