c77313e3c37f626e7f470b327e03b664d5b69094
Jonathan Perkins <jperkin4@uab.edu>
2024-07-19 00:39:05 -0500

    Changing color scheme

--- src/colormap.js
+++ src/colormap.js
@@ -6,21 +6,21 @@
  */
 function getColor(sortName) {
     var colors = {
-        insertionSort: "#ffb381",
-        mergeSort: "#ff8a01",
-        quickSort: "#ff5558",
-        selectionSort: "#bd9fd1",
-        heapSort: "#390f37",
-        countingSort: "#3a4e9e",
-        radixSort: "#01308a",
-        bubbleSort: "#815288",
+        insertionSort: "#EFCA00",
+        mergeSort: "#FE433C",
+        quickSort: "#F31D64",
+        selectionSort: "#A224AD",
+        heapSort: "#6A38B3",
+        countingSort: "#3C5081",
+        radixSort: "#0095EF",
+        bubbleSort: "#43C68B",
 
         python: "#3572a5",
         java: "#b07219",
         cpp: "#f34b7d",
         javaScript: "#f1e05a",
 
-        other: "#83828b"
+        other: "#BCBCBC"
     };
 
     var color = colors[sortName];
